cff8f78736bec77e4c72caec330acfecf5408e57
mathdothings <jhonata.prodrigues@gmail.com>
2024-02-03 17:30:25 -0300

    fix: autoloader rewrite

--- src/signup-process.php
+++ src/signup-process.php
@@ -1,9 +1,5 @@
 <?php
 
-// spl_autoload_register(function (string $class) {
-//     require_once __DIR__ . "/$class.php";
-// });
-
 spl_autoload_register(function ($class) {
     require_once __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 });

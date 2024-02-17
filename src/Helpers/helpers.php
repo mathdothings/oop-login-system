c21719367ca1a6d3fa8d223d54ab421446003d27
mathdothings <jhonata.prodrigues@gmail.com>
2024-02-17 20:06:29 -0300

    feat: pretty print array

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -6,3 +6,8 @@ function basePath($path = ""): string
 {
     return ROOT_PATH . $path;
 }
+
+function prettyPrint(array $array): int
+{
+    return print "<pre>" . print_r($array, true) . "</pre>";
+}

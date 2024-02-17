dd14db60c1e83d394586efde14ea580a593e417e
mathdothings <jhonata.prodrigues@gmail.com>
2024-02-17 18:18:02 -0300

    feat: define absolute views path

--- config.php
+++ config.php
@@ -1,2 +1,7 @@
 <?php
 const ROOT_PATH = __DIR__ . '/../';
+define(
+    'VIEWS_PATH',
+    "{$_SERVER['REQUEST_SCHEME']}" . '://' .
+        "{$_SERVER['SERVER_NAME']}" . dirname("{$_SERVER['REQUEST_URI']}")
+);

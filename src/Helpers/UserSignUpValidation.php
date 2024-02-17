3d37e6d9e682665fcafb237826c43f6b51a7f3b7
mathdothings <jhonata.prodrigues@gmail.com>
2024-02-17 18:36:54 -0300

    refactor: validade repeat password moved to child class

--- src/Helpers/UserSignUpValidation.php
+++ src/Helpers/UserSignUpValidation.php
@@ -13,11 +13,24 @@ class UserSignUpValidation extends UserValidation
         return false;
     }
 
+    function validateRepeatPassword(string $password, string $repeatPassword): bool
+    {
+        if ($password !== $repeatPassword)
+            $this->BrokenRules[] = "Passwords don't match!";
+
+        if (!$this->BrokenRules) return true;
+        return false;
+    }
+
     function validate(array $userSignUpInput): bool | array
     {
         $this->validateName($userSignUpInput['name']);
         $this->validateEmail($userSignUpInput['email']);
-        $this->validatePassword($userSignUpInput['password'], $userSignUpInput['repeatPassword']);
+        $this->validatePassword($userSignUpInput['password']);
+        $this->validateRepeatPassword(
+            $userSignUpInput['password'],
+            $userSignUpInput['repeatPassword']
+        );
 
         if (!$this->BrokenRules) return true;
         return $this->BrokenRules;
